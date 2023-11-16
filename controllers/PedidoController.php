c082e02eb849e7ac383076bdfc77d2b6008a6b2f
danielveg4 <dvgilles@gmail.com>
2023-11-16 17:26:13 +0100

    16

--- controllers/PedidoController.php
+++ controllers/PedidoController.php
@@ -2,4 +2,9 @@
 
 require_once 'models/pedido.php';
 
-class 
+class PedidoController{
+    public function hacer(){
+        //va a cargar la vista hacer.php
+        require_once 'views/pedido/hacer.php';
+    }
+}
